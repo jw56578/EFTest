b663a643c4b8c1ffea57e646291517f70c5a3f2d
jw56578 <jw56578@gmail.com>
2015-07-25 15:33:38 -0500

    add stuff

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFD.Data;
+using EFD.Entity;
+using HibernatingRhinos.Profiler.Appender.EntityFramework;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        //install entity framework power tools so that you can right click on any class file and create a model schema from it
+        //when any EF code gets run as in GetPeople, it will automatically create the database and tables
+        //to find the DB in the explorer of visual studio:
+        //click Add Sql Server and enter (localdb)\mssqllocaldb
+        //thee database will be named by default as the namespace of your context class, Whatever.Whatever1.SomethingContext
+        //there may be several random databases and instances so don't get confused
+        static void Main(string[] args)
+        {
+           //EntityFrameworkProfiler.Initialize();
+            var emp = new Employee();
+            //InsertPerson();
+            //UpdatePerson();
+            //GetPeople();
+           // CreateObjectGraph();
+            GetParentObjectOnlyByFind();
+            Console.ReadLine();
+        }
+
+        public static void GetParentObjectOnlyByWhere()
+        {
+            using (var context = new EFDContext())
+            {
+                var people = context.People.Find(6);
+
+            }
+        }
+
+
+        //get a parent object with Find. This object should have a child object property that we don't want retrieved
+        //I don't see anything in sql profiler retrieving anything about the Address collection
+        public static void GetParentObjectOnlyByFind()
+        {
+            using (var context = new EFDContext())
+            {
+                var people = context.People.Find(6);
+
+            }
+        }
+
+        //create new parent object and new children objects
+        public static void CreateObjectGraph()
+        {
+            using (var context = new EFDContext())
+            {
+                var p = new Person() {FirstName = "Bob", LastName = "Rogden"};
+                p.Addresses.Add((new Address(){Street = "main street"}));
+                //do you have to specify the person id or Person peroperty on the address or will it just work?
+                //COOL, it just works
+                context.People.Add(p);
+                context.SaveChanges();
+            }
+
+        }
+
+        public static void UpdatePerson()
+        {
+            using (var context = new EFDContext())
+            {
+                var per = context.People.FirstOrDefault();
+                per.LastName = DateTime.Now.ToString();
+                context.SaveChanges();
+            }
+
+        }
+        public static void InsertPerson()
+        {
+            using (var context = new EFDContext())
+            {
+                context.People.Add(new Person() {FirstName = "Bob", LastName = "Rogden"});
+                context.SaveChanges();
+            }
+
+        }
+
+        public static void GetPeople()
+        {
+            using (var context = new EFDContext())
+            {
+                var people = context.People.ToList();
+                foreach (var person in people)
+                {
+                    Console.WriteLine(person.FirstName + " " + person.LastName);
+                }
+            }
+        }
+    }
+}
---
+++ EFD.Data/Class1.cs
@@ -0,0 +1,84 @@
+﻿using EFD.Entity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFD.Data
+{
+    //this is kinda like the concept of a database, but it doesn't have to be
+    public class EFDContext:DbContext  
+    {
+        //this is kinda like the concept of a database table, but it doesn't have to be
+        public DbSet<Person> People { get; set; }
+    }
+}
+namespace EFD.Entity
+{
+    public class Person
+    {
+        public Person()
+        {
+            //its a decent thing to do, to initialize things in the contructor, but if its a virtual method problems can occur
+            Addresses = new List<Address>();
+        }
+
+        //all entities need an Id field called "Id" or Type + "Id" or use the Key attribute
+        [Key]
+        public int Id { get; set; } // PersonId
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //virtual is used for something having to do with lazy loading
+        public virtual ICollection<Address> Addresses { get; set; }
+        public virtual PersonDetail PersonDetail { get; set; }
+    }
+
+    public class Employee:Person
+    {
+        public readonly bool IsGood = false;
+        public Employee()
+        {
+            IsGood = true;
+        }
+
+        //public override ICollection<Address> Addresses
+        //{
+        //    get { return null; }
+        //    set
+        //    {
+        //        if(!IsGood)
+        //            throw new Exception("whoops, virtual calls in contructors aren't good!!!");
+
+        //    }
+        //}
+    }
+
+    public class Address
+    {
+        public int Id { get; set; }
+        public virtual Person Person { get; set; }
+        public int PersonId { get; set; }
+        public string Street { get; set; }
+    }
+    //this is a one to zero/one one relationsship, you have to define which is the parent object and where the foriegn key goes
+    public class PersonDetail
+    {
+        [Key,ForeignKey("Person")] //this needs to be the name of the Property that has the parent object, which in this case is named the same as the Type
+        public int PersonId
+        {
+            get; set;
+            
+        }
+        public int Id { get; set; }
+        public Person Person { get; set; }
+
+    }
+
+
+}
